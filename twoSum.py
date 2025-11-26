4e9c145327139b4cca7c368f92702ff1a4e6865c
babysowmya <147791943+babysowmya@users.noreply.github.com>
2025-11-26 14:26:38 +0530

    Create twoSum.py

---
+++ twoSum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, arr, target):
+        left = 0
+        right = len(arr) - 1
+        while left < right:
+            s = arr[left] + arr[right]  
+            if s == target:
+                return [left + 1, right + 1]
+            elif s < target:
+                left += 1
+            else:
+                right -= 1
+        return [-1, -1]
