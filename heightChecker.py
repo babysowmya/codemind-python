6a461e1ba6aa5d4b016d928b7fc4acfaa26df18f
babysowmya <147791943+babysowmya@users.noreply.github.com>
2025-12-17 18:58:19 +0530

    Create heightChecker.py

---
+++ heightChecker.py
@@ -0,0 +1,8 @@
+class Solution:
+    def heightChecker(self, heights: List[int]) -> int:
+        a = sorted(heights)
+        c = 0
+        for i in range(len(heights)):
+            if heights[i]!=a[i]:
+                c = c+1
+        return c
